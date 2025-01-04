1042bee596a3fc750c904abdee179cec2e6c57f1
sixtusdeveloper <sixtusushrey@gmail.com>
2025-01-04 23:23:14 +0100

    updated the app component

---
+++ src/app/employee.service.ts
@@ -0,0 +1,91 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Employee } from './employee';
+import { environment } from '../environments/environment';
+
+@Injectable({providedIn: 'root'})
+export class EmployeeService {
+  private apiServerUrl = environment.apiBaseUrl;
+
+  constructor(private http: HttpClient){}
+
+  public getEmployees(): Observable<Employee[]> {
+    return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+  }
+
+  public addEmployee(employee: Employee): Observable<Employee> {
+    return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+  }
+
+  public updateEmployee(employee: Employee): Observable<Employee> {
+    return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+  }
+
+  public deleteEmployee(employeeId: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+  }
+}
+
+
+// Need some improvements to be implemented
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { Employee } from './employee';
+// import { environment } from '../environments/environment';
+
+// @Injectable({providedIn: 'root'})
+// export class EmployeeService {
+//   private apiServerUrl = environment.apiBaseUrl;
+
+//   constructor(private http: HttpClient){}
+
+//   public getEmployees(): Observable<Employee[]> {
+//     return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+//   }
+
+//   public addEmployee(employee: Employee): Observable<Employee> {
+//     return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+//   }
+
+//   public updateEmployee(employee: Employee): Observable<Employee> {
+//     return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+//   }
+
+//   public deleteEmployee(employeeId: number): Observable<void> {
+//     return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+//   }
+// }
+
+
+
+// Earlier version of the code not working as expected
+// import { Injectable } from '@angular/core';
+// import { HttpClient } from '@angular/common/http';
+// import { Observable } from 'rxjs';
+// import { Employee } from './employee';
+// import { environment } from '../environments/environment';
+
+// @Injectable({providedIn: 'root'})
+// export class EmployeeService {
+//   private apiServerUrl = environment.apiBaseUrl;
+
+//   constructor(private http: HttpClient){}
+
+//   public getEmployees(): Observable<Employee[]> {
+//     return this.http.get<Employee[]>(`${this.apiServerUrl}/employee/all`);
+//   }
+
+//   public addEmployee(employee: Employee): Observable<Employee> {
+//     return this.http.post<Employee>(`${this.apiServerUrl}/employee/add`, employee);
+//   }
+
+//   public updateEmployee(employee: Employee): Observable<Employee> {
+//     return this.http.put<Employee>(`${this.apiServerUrl}/employee/update`, employee);
+//   }
+
+//   public deleteEmployee(employeeId: number): Observable<void> {
+//     return this.http.delete<void>(`${this.apiServerUrl}/employee/delete/${employeeId}`);
+//   }
+// }
